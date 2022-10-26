70b650d18dd1b170dff29eeea3609defcc759405
Shrishti-Singh-15 <shrishtisingh.vns2001@gmail.com>
2022-10-26 15:07:52 +0530

    commit

---
+++ Day_3/Project_Treasure_Island.py
@@ -0,0 +1,42 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.\nYour Mission is to find the treasure.\n")
+direction = input('You\'re at a crossword, where do you want to go? Type "left" or "right".\n').lower()
+
+if direction == "left":
+    dir = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across\n').lower()
+    if dir == "wait":
+        door = input("You arrived at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which door do you choose?\n").lower()
+        if door == "red":
+            print("It's a room of fire. Game Over.\n")
+        elif door == "yellow":
+            print("You found the treasure! You Win!\n")
+        elif door == "blue":
+            print("It's a room of beasts. Game Over.\n")
+        else:
+            print("You chose a door that doesn't exist. Game Over.\n")
+
+    else:
+        print("You got attacked by an angry trout. Game Over.\n")
+else:
+    print("You fell into a hole. Game Over.\n")
